6f8e52ac1007b6b9f7f74769829d5d3955eeede7
Grant Duffy <grantd@buchanan-a.com>
2019-12-22 01:11:18 -0800

    Added Levels tracking

--- pyaudio_test.py
+++ pyaudio_test.py
@@ -28,9 +28,15 @@ bass = np.zeros(buffer_size)
 mid = np.zeros(buffer_size)
 treble = np.zeros(buffer_size)
 
+ax1 = plt.subplot(211)
 fft_line = plt.plot(freq, amp)[0]
 plt.ylim(0, 10)
 
+ax2 = plt.subplot(212)
+bass_line = plt.plot([], label='Bass')[0]
+mid_line = plt.plot([], label='Mid Range')[0]
+treble_line = plt.plot([], label='Treble')[0]
+plt.legend()
 
 
 def audio_worker():
@@ -42,14 +48,24 @@ def audio_worker():
         mid = np.average(amp[(freq < mid_treble_freq) * (freq > bass_mid_freq)])
         treble = np.average(amp[(freq > mid_treble_freq)])
         levels.append((bass, mid, treble))
+    stream.close()
 
 
 def animate(i):
+    bass_arr, mid_arr, treble_arr = np.array(levels).T
+    frames = np.arange(len(bass_arr))
     fft_line.set_ydata(amp)
+    bass_line.set_data(frames, bass_arr)
+    mid_line.set_data(frames, mid_arr)
+    treble_line.set_data(frames, treble_arr)
+    ax2.set_xlim(0, len(bass_arr))
+    ax2.set_ylim(0, np.max([bass_arr, mid_arr, treble_arr]))
     return fft_line,
 
 
 audio_thread = Thread(target=audio_worker)
 audio_thread.start()
-ani = animation.FuncAnimation(plt.gcf(), animate, blit=True)
+ani = animation.FuncAnimation(plt.gcf(), animate)
 plt.show()
+
+run = False
