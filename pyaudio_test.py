40d4325c59d51555183443a77df98ba847f5c6e5
Grant Duffy <grantd@buchanan-a.com>
2019-12-22 00:53:31 -0800

    Added threading

--- pyaudio_test.py
+++ pyaudio_test.py
@@ -1,6 +1,8 @@
 import pyaudio
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib import animation
+from threading import Thread
 
 buffer_size = 512
 sample_rate = 16000
@@ -18,29 +20,36 @@ stream = pa.open(
 freq = np.abs(np.fft.fftfreq(buffer_size, d=1.0 / sample_rate))
 bass_mid_freq = 250.0
 mid_treble_freq = 4000.0
+run = True
+levels = []
+wave = np.zeros(buffer_size)
+amp = np.zeros(buffer_size)
+bass = np.zeros(buffer_size)
+mid = np.zeros(buffer_size)
+treble = np.zeros(buffer_size)
 
-while True:
-    wave = np.fromstring(stream.read(buffer_size), np.float32)
-    amp = np.abs(np.fft.fft(wave))
-    bass = np.average(amp[freq < bass_mid_freq])
-    mid = np.average(amp[(freq < mid_treble_freq) * (freq > bass_mid_freq)])
-    treble = np.average(amp[(freq > mid_treble_freq)])
+fft_line = plt.plot(freq, amp)[0]
+plt.ylim(0, 10)
 
-    plt.subplot(311)
-    plt.gca().clear()
-    plt.title('Audio Wave')
-    plt.ylabel('Value')
-    plt.ylabel('Samples')
-    plt.plot(wave)
-    plt.subplot(312)
-    plt.gca().clear()
-    plt.title('Frequencies')
-    plt.xlabel('Magnitude')
-    plt.ylabel('Frequency')
-    plt.plot(np.log10(np.abs(freq)), amp)
-    plt.subplot(313)
-    plt.gca().clear()
-    plt.title('Levels')
-    plt.bar([0, 1, 2], [bass, mid, treble])
-    plt.xticks([0, 1, 2], ['Bass', 'Mid Range', 'Treble'])
-    plt.pause(0.01)
+
+
+def audio_worker():
+    global wave, amp, bass, mid, treble, levels
+    while run:
+        wave = np.fromstring(stream.read(buffer_size), np.float32)
+        amp = np.abs(np.fft.fft(wave))
+        bass = np.average(amp[freq < bass_mid_freq])
+        mid = np.average(amp[(freq < mid_treble_freq) * (freq > bass_mid_freq)])
+        treble = np.average(amp[(freq > mid_treble_freq)])
+        levels.append((bass, mid, treble))
+
+
+def animate(i):
+    fft_line.set_ydata(amp)
+    return fft_line,
+
+
+audio_thread = Thread(target=audio_worker)
+audio_thread.start()
+ani = animation.FuncAnimation(plt.gcf(), animate, blit=True)
+plt.show()
