0402b9d0852d2ad671dd17d8eab710884807ad53
Grant Duffy <grantd@buchanan-a.com>
2019-12-22 20:26:35 -0800

    Minimally working

---
+++ artnet_client.py
@@ -0,0 +1,22 @@
+import socket, neopixel, machine
+
+n_led = 50
+np = neopixel.NeoPixel(machine.Pin(4), 50)
+
+
+def run():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.bind(('192.168.0.34', 6454))
+        while True:
+            data, addr = s.recvfrom(1024)
+            header = data[:18]
+            body = data[18:]
+            # print(len(data), addr)
+            if len(data) == 530:
+                for i in range(n_led):
+                    np[i] = (body[i * 3 + 1], body[i * 3], body[i * 3 + 2])
+                np.write()
+    except:
+        s.close()
+        raise
--- pyaudio_test.py
+++ pyaudio_test.py
@@ -4,13 +4,19 @@ from matplotlib import pyplot as plt
 from matplotlib import animation
 from threading import Thread, Lock
 from matplotlib.colors import hsv_to_rgb
+import socket
+from time import sleep
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 buffer_size = 512
 sample_rate = 16000
 led_n = 50
-bass_gain = 6.0
+bass_gain = 0.5
 mid_gain = 2.0
 treble_gain = 10.0
+gamma = 3.0
+fade_rate = 0.003
 
 pa = pyaudio.PyAudio()
 stream = pa.open(
@@ -31,24 +37,17 @@ run = True
 levels = []
 wave = np.zeros(buffer_size)
 amp = np.zeros(buffer_size)
-bass = np.zeros(buffer_size)
-mid = np.zeros(buffer_size)
-treble = np.zeros(buffer_size)
+bass = 0
+mid = 0
+treble = 0
 hsv_values = np.ones((led_n, 3))
 hsv_values[:, 0] = np.linspace(0, 1, led_n)
+rgb_values = np.zeros((led_n, 3))
 
 scat = plt.scatter(np.arange(led_n) * 3 / 50, np.zeros(led_n), c=np.zeros((led_n, 3)))
 levels_line = plt.plot([0, 1, 2, 3], [0, 0, 0, 0])[0]
-plt.ylim(-0.1, 1)
-
-# ax1 = plt.subplot(211)
-# fft_line = plt.plot(freq, amp)[0]
-# plt.ylim(0, 10)
-# ax2 = plt.subplot(212)
-# bass_line = plt.plot([], label='Bass')[0]
-# mid_line = plt.plot([], label='Mid Range')[0]
-# treble_line = plt.plot([], label='Treble')[0]
-# plt.legend()
+hsv_line = plt.plot(np.linspace(0, 3, led_n), hsv_values[:, 0])[0]
+plt.ylim(-0.1, 1.1)
 
 
 def audio_worker():
@@ -59,45 +58,56 @@ def audio_worker():
         bass = np.average(amp[freq < bass_mid_freq])
         mid = np.average(amp[(freq < mid_treble_freq) * (freq > bass_mid_freq)])
         treble = np.average(amp[(freq > mid_treble_freq)])
+        # print(bass.shape, mid.shape, treble.shape)
         levels.append((bass, mid, treble))
     stream.close()
 
 
+def udp_worker():
+    global rgb_values, hsv_values
+    while run:
+        current_levels = np.array([bass * bass_gain, mid * mid_gain, treble * treble_gain, bass * bass_gain])
+        lightness = np.interp(np.linspace(0, 3, led_n), [0, 1, 2, 3], current_levels)
+
+        hsv_values[:, 0] += fade_rate
+        hsv_values[:, 0][hsv_values[:, 0] > 1] -= 1
+
+        rgb_values = hsv_to_rgb(hsv_values)
+        rgb_values[lightness <= 0.5] *= (lightness[lightness <= 0.5] * 2).reshape((-1, 1))
+
+        a = (lightness[lightness > 0.5] * 2 - 1).reshape((-1, 1))
+        rgb_values[lightness > 0.5] = (1 - a) * rgb_values[lightness > 0.5] + a * np.ones((led_n, 3))[lightness > 0.5]
+        rgb_values[rgb_values > 1.0] = 1.0
+        rgb_values[rgb_values < 0.0] = 0.0
+        rgb_values = rgb_values ** gamma
+
+        data = np.zeros(512, dtype=np.uint8)
+        data[:led_n * 3] = (rgb_values.flatten() * 255).astype(np.uint8)
+        packet = b'Art-Net\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' + data.tobytes()
+
+        s.sendto(packet, ('192.168.0.34', 6454))
+        sleep(1/20)
+
+
 def animate(i):
-
-    current_levels = [bass * bass_gain, mid * mid_gain, treble * treble_gain, bass * bass_gain]
-    lightness = np.interp(np.linspace(0, 3, led_n), [0, 1, 2, 3], current_levels)
-    # scat.set_color(np.ones((led_n, 3)) * lightness)
-    rgb_values = hsv_to_rgb(hsv_values)
-    rgb_values[lightness <= 0.5] *= (lightness[lightness <= 0.5] * 2).reshape((-1, 1))
-    # rgb_values[lightness > 0.5] *= (1 - lightness[lightness > 0.5] * 2).reshape((-1, 1)) \
-    #                                + np.ones((led_n, 3))[lightness > 0.5]
-
-    a = (lightness[lightness > 0.5] * 2 - 1).reshape((-1, 1))
-    print(a.shape, rgb_values[lightness > 0.5].shape)
-    rgb_values[lightness > 0.5] = (1 - a) * rgb_values[lightness > 0.5] + a * np.ones((led_n, 3))[lightness > 0.5]
-    rgb_values[rgb_values > 1.0] = 1.0
-    rgb_values[rgb_values < 0.0] = 0.0
-    print(np.max(rgb_values), np.min(rgb_values))
+    current_levels = np.array([bass * bass_gain, mid * mid_gain, treble * treble_gain, bass * bass_gain])
     scat.set_color(rgb_values)
     levels_line.set_ydata(current_levels)
+    hsv_line.set_ydata(hsv_values[:, 0])
     return levels_line, scat
-
-    # bass_arr, mid_arr, treble_arr = np.array(levels).T
-    # frames = np.arange(len(bass_arr))
-    # fft_line.set_ydata(amp)
-    # bass_line.set_data(frames, bass_arr)
-    # mid_line.set_data(frames, mid_arr)
-    # treble_line.set_data(frames, treble_arr)
-    # ax2.set_xlim(0, len(bass_arr))
-    # ax2.set_ylim(0, np.max([bass_arr, mid_arr, treble_arr]))
-    # return fft_line,
 
 
 audio_thread = Thread(target=audio_worker)
 audio_thread.start()
 
-ani = animation.FuncAnimation(plt.gcf(), animate)
-plt.show()
+udp_thread = Thread(target=udp_worker)
+udp_thread.start()
 
-run = False
+# ani = animation.FuncAnimation(plt.gcf(), animate)
+# plt.show()
+
+while True:
+    try:
+        sleep(10)
+    except KeyboardInterrupt:
+        run = False
